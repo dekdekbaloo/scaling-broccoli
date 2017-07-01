8b3fd603acc6a5345d9bfa2a0b9056864d527dd9
Jessada Trirongkit <jessada.trirongkit@gmail.com>
2017-07-01 13:27:15 +0700

    Simple webpack, dev-server, babel, js web app starter.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: path.resolve(__dirname, 'node_modules'),
+        loader: 'babel-loader',
+        options: {
+          presets: [['es2015', { modules: false }]]
+        }
+      }
+    ]
+  }
+}
